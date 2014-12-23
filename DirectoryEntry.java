b0f2569e165a3488ebd5f122556ec58310a5a653
Sean Staz <seanstaz52@gmail.com>
2014-12-23 00:41:32 -0600

    Create DirectoryEntry.java

---
+++ DirectoryEntry.java
@@ -0,0 +1,21 @@
+
+
+/**
+ * A class representing a directory in a file system.
+ * @author rhodes
+ *
+ */
+public class DirectoryEntry extends FileSystemObject{
+
+	/**
+	 * Construct a Directory entry with the given name.
+	 * The size will be zero.
+	 * @param name the directory name
+	 */
+	public DirectoryEntry(String name){
+		
+		super(name, 0);
+	}
+	
+
+}
